b459ee53cc4573c043727cf942dc3b577ef4ecb5
VoleTT <sarge.tenebrae@yahoo.com>
2015-11-14 00:23:56 +0300

    Очень важное изменение в бидлокоде

--- Drums/MainForm.Designer.cs
+++ Drums/MainForm.Designer.cs
@@ -14,6 +14,7 @@ namespace Drums
 		/// Designer variable used to keep track of non-visual components.
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -36,13 +37,24 @@ namespace Drums
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.button1 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(107, 119);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Щёки";
+			this.button1.UseVisualStyleBackColor = true;
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.button1);
 			this.Name = "MainForm";
 			this.Text = "Drums";
 			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainFormFormClosed);
